c90fe3c077cb2b7b4b9e1fa20eba653c48127aef
Allen Thomas Varghese <allentv4u@gmail.com>
2020-01-21 19:23:55 +0000

    Adding views

---
+++ todoapp/todos/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path, include
+
+from .views import LandingPage
+
+urlpatterns = [
+    path("", LandingPage.as_view(), name="index"),
+]
--- todoapp/todos/views.py
+++ todoapp/todos/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Todo
+from django.views import View
+
+
+class LandingPage(View):
+    def get(self, request):
+        todos = Todo.objects.order_by("-id")
+        context = {"todos": todos}
+
+        return render(request, "todos/index.html", context=context)
