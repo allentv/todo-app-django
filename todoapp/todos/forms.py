1e4231d58a3522ff606c928fd7ba7f6c57f774d1
Allen Thomas Varghese <allentv4u@gmail.com>
2020-01-23 08:19:00 +0000

    Add bootstrap styling

--- todoapp/todos/forms.py
+++ todoapp/todos/forms.py
@@ -3,5 +3,8 @@ from django.forms import Form, CharField, TextInput
 
 class TodoForm(Form):
     todo = CharField(
-        max_length=40, widget=TextInput(attrs={"placeholder": "Enter a task to do"})
+        max_length=40,
+        widget=TextInput(
+            attrs={"placeholder": "Enter a Todo item", "class": "form-control"}
+        ),
     )
